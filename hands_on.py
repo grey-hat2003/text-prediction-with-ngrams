1b7c75a6a5101a1b3165d00d7d6b1008739f5b67
grey-hat2003 <62413157+grey-hat2003@users.noreply.github.com>
2021-04-04 16:11:28 +0530

    added local files

---
+++ hands_on.py
@@ -0,0 +1,89 @@
+import os
+
+data_base = 'abc.txt'
+
+first_possible_words = {}
+second_possible_words = {}
+transitions = {}
+
+def add_to_dict(dict, key, value):
+    if key not in dict:
+        dict[key] = []
+
+    dict[key].append(value)
+
+def get_next_probability(given_list):   #returns dictionary
+    probability_dict = {}
+    given_list_length = len(given_list)
+
+    for item in given_list:
+        probability_dict[item] = probability_dict.get(item, 0) + 1
+
+    for key, value in probability_dict.items():
+        probability_dict[key] = value / given_list_length
+
+    return probability_dict
+
+def trainMarkovModel():
+    for line in open(data_base):   #iterating through every line
+        tokens = line.rstrip().lower().split()   #made a list of all the words in the line
+        tokens_length = len(tokens)    #took the length of this list (no. of words in that line)
+
+        for i in range(tokens_length):
+            token = tokens[i]   #taking a single word
+
+            if i == 0:
+                first_possible_words[token] = first_possible_words.get(token, 0) + 1 
+                #calculating the frequencies of all the first words, eg. {'i': 5, 'how': 4, 'this': 3}
+                #get function returns the value zero if the key doesn't exists, otherwise increments it by one
+                print(first_possible_words)
+
+
+            else:
+                prev_token = tokens[i - 1]
+
+                if i == tokens_length - 1:
+                    add_to_dict(transitions, (prev_token, token), 'END')
+
+                if i == 1:
+                    add_to_dict(second_possible_words, prev_token, token)
+
+                else:
+                    prev_prev_token = tokens[i - 2]
+                    add_to_dict(transitions, (prev_prev_token, prev_token), token)
+
+    first_possible_words_total = sum(first_possible_words.values())
+
+'''
+    for key, value in first_possible_words.items():
+        first_possible_words[key] = value / first_possible_words_total
+
+    for prev_word, next_word_list in second_possible_words.items():
+        second_possible_words[prev_word] = get_next_probability(next_word_list)
+
+    for word_pair, next_word_list in transitions.items():
+        transitions[word_pair] = get_next_probability(next_word_list)
+
+def next_word(tpl):
+    #print(transitions)
+    if(type(tpl) == str):   #it is first word of string.. return from second word
+        d = second_possible_words.get(tpl)
+
+        if (d is not None):
+            return list(d.keys())
+
+    elif(type(tpl) == tuple): #incoming words are combination of two words.. find next word now based on transitions
+        d = transitions.get(tpl)
+
+        if(d == None):
+            return []
+        return list(d.keys())
+
+    return None #wrong input.. return nothing
+'''
+
+trainMarkovModel()  #generate first, second words list and transitions
+
+########## demo code below ################
+
+print("This is a n-gram text predictor. Type in anything and you'll get suggestions on which words to use next. \nPress TAB to use the first word (the word with highest probablity), or type in your own word :)\n")
---
+++ selfcode.py
@@ -0,0 +1,120 @@
+import os
+import re
+
+corpus = 'abc.txt'
+
+first_words = {}
+second_words = {}
+transitions = {}
+
+def add_to_dict(diction, key, val):
+    if key not in diction:
+        dict[key] = []
+        #making an empty list as a value for the new key
+    
+    dict[key].append(val)
+
+def update_dict(diction, key, val):
+    if key not in diction:
+        diction[key] = {}
+
+    diction[key] = diction.get(val, 0) + 1
+    # no need of [value]
+
+
+def calculate_probab(listy):
+    probab_dict = {}
+    listy_length = len(listy)
+
+    for element in listy:
+        probab_dict[element] = probab_dict.get(element, 0) + 1
+    
+    # for key, value in probab_dict.items():
+    #     probab_dict[key] = value/listy_length
+
+    return probab_dict
+    
+def train_markov():
+#this function is the real logic part. We are implementing markov chains here.
+    data_base = open(corpus, 'r')
+    #opening our corpus in read mode
+
+    for sentence in data_base:       #scanning through every line in our corpus
+        sentence = re.sub(r"[,.\"\'\\!@#$%^&*(){}?/;:<>+=-]", "", sentence)   #clearing the sentence from any punctuation marks
+        words = sentence.strip().lower().split()    #extracting words and make a list of the same
+        no_of_words = len(words)    #the size of this list gives us the number of words in that sentence
+
+        for x in range(no_of_words):
+            #we iterate through every word of a sentence
+            #word variable is an individual word and x in the index or word number
+            word = words[x]
+
+            if(x == 0):
+                first_words[word] = first_words.get(word, 0) + 1
+                #we make a dict of all the possible first words as the key and calculate its frequency as the value
+
+            else:
+                prev_word = words[x-1] #we extract the previous word 
+
+                if x == no_of_words - 1:
+                    add_to_dict(transitions, (prev_word, word), ' :( ')
+
+                if x == 1:
+                    add_to_dict(second_words, prev_word, word)
+
+                else:
+                    second_prev_word = words[x-2]
+                    add_to_dict(transitions, (second_prev_word, prev_word), word)
+
+    #total_first_words = sum(first_words.values())
+
+    #probability 
+
+    # for key, value in first_words.items():
+    #     first_words[key] = value / total_first_words  
+        #doing this gives us the relative probability after processing every first word and its possible subsequent words
+
+    for prev_word, next_words in second_words.items(): 
+        #prev_word is the key and next_words is a list type value 
+        second_words[prev_word] = calculate_probab(next_words)
+
+    for word_group, next_words in transitions.items():
+        #word_group is the key and next_words is a list type value
+        transitions[word_group] = calculate_probab(next_words)
+
+def first_words_sort():
+    #sorts the first words dictionary
+    return sorted(first_words, key = first_words.get, reverse = True)
+
+
+def update_corpus(sentence):
+    f = open(corpus, "a")
+    f.write("\n" + sentence)
+    f.close()
+
+
+def suggestions(in_put):
+    if(type(in_put) == str):
+    #if the input is just a single word
+        suggestion = second_words.get(in_put)
+        #trying to suggest a 
+        if(suggestion is not None):
+            return sorted(suggestion, key = suggestion.get, reverse = True)
+
+    if(type(in_put) == tuple):
+    #if the input is a string of words
+        suggestion = transitions.get(in_put)
+
+        if(suggestion == None):
+            return []
+        return sorted(suggestion, key = suggestion.get, reverse = True)
+
+    return []
+
+train_markov()
+
+print(first_words)
+print()
+print(second_words)
+print()
+print(transitions)
